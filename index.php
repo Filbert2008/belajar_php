ab2b8f87bfff732c729bb7e9d2db4cf3730bd49b
filbert2008 <ana.rika24@smk.belajar.id>
2025-01-09 10:57:37 +0700

    menghilangkan bug

--- index.php
+++ index.php
@@ -9,13 +9,7 @@
     <h3>
         <?php
         echo "hello world!!! otw tidur 5 menit coy";
-        echo "GAS KITA TIDUR 5 MENIT";
-        echo "LETS GOOOOOOOO";
-        echo "Tapi tidak dianjurkan ya dek ya";
-        echo "Kurang primo hiiiiiiiiiiiiks";
-        echo "sahabat promag";
-        echo "otw melawan pyro dragon";
-        echo "testing";
+       
         ?>
     </h3>
 </body>
