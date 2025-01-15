348edccdcd35435412b3754e5cd8b38d6e7626b5
filbert2008 <ana.rika24@smk.belajar.id>
2025-01-15 11:01:20 +0700

    p

--- index.php
+++ index.php
@@ -10,7 +10,10 @@
         <?php
         echo "hello world!!! otw tidur 5 menit coy";
         echo "pppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp";
-        
+
+        $var1=9999;
+         echo 'nilai dari variabel var1 adalah ' number_format($var1,0',',',')
+         echo "nilai dari variabel var1 adalah " $var1"<br/>";
         ?>
     </h3>
 </body>
